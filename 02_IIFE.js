768830fb48cfa0b5d7191e13fcb4bb70db04e53d
Devashish <devashishsharma081@gmail.com>
2023-10-26 23:15:35 +0530

    IIFE

--- 02_IIFE.js
+++ 02_IIFE.js
@@ -1,1 +1,9 @@
 //Immediately invoked function expression
+// IIFE is used to avoid the problem caused by global scope pollution
+(function chai() {
+  console.log(`Hello Everyone`);
+})();
+
+((name) => {
+  console.log(`DB connect to ${name}`);
+})("Devashish");
