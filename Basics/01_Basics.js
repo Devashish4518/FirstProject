e65ee4e39f80e19d19abc5d4712b329858300f33
Devashish <devashishsharma081@gmail.com>
2023-11-11 12:55:42 +0530

    upd

--- Basics/01_Basics.js
+++ Basics/01_Basics.js
@@ -1,37 +1,34 @@
 function one() {
-    const username = "Devashish"
-    
-    function two(){
-        const website = "Twitter"
-        console.log(username);
-    }
-    //console.log(website); //out of scope two
-   // two()
+  const username = "Devashish";
+
+  function two() {
+    const website = "Twitters";
+    console.log(username);
+  }
+  //console.log(website); //out of scope two
+  // two()
 }
 //one()
 
-
 if (true) {
-    const username = "Devashish"
-    if (username === "Devashish") {
-        const website = " Youtube"
-        console.log(username + weebsite);
-    }
-    //console.log(website);
+  const username = "Devashish";
+  if (username === "Devashish") {
+    const website = " Youtube";
+    console.log(username + weebsite);
+  }
+  //console.log(website);
 }
 //console.log(username);
-
 
 //+++++++++++++++++++++++ Special Case ++++++++++++++++++
 
 console.log(addone(45));
-function addone(num){
-    return num + 1
+function addone(num) {
+  return num + 1;
 }
-
 
 //we cannot access the function before in this case
-addTwo()
-const addTwo = function(num){
-    return num + 2
-}
+addTwo();
+const addTwo = function (num) {
+  return num + 2;
+};
