bee1a9809e6bc679502d5ca58ef7ab19f63b5602
Devashish <devashishsharma081@gmail.com>
2023-11-17 23:06:22 +0530

    loop

---
+++ iterations/one.js
@@ -0,0 +1,21 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//   const element = i;
+//   console.log(element);
+// }
+
+// for (let index = 1; index <= 20; index++) {
+//   if (index == 5) {
+//     console.log(`Detected 5`);
+//     break;
+//   }
+//   console.log(`Value of i is ${index}`);
+// }
+for (let index = 1; index <= 20; index++) {
+  if (index == 5) {
+    console.log(`Detected 5`);
+    continue;
+  }
+  console.log(`Value of i is ${index}`);
+}
